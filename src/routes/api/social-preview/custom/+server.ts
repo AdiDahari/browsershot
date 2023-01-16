f2a40f4b12b8bb10713689b516f43d82be9055a9
AdiDahari <dahariadi16@gmail.com>
2023-01-16 12:15:31 +0200

    added fetch functionallity

--- src/routes/api/social-preview/custom/+server.ts
+++ src/routes/api/social-preview/custom/+server.ts
@@ -1,6 +1,27 @@
 import type {RequestHandler} from './$types'
 import {PUBLIC_API_BASE_URL} from '$env/static/public'
 import {API_KEY} from '$env/static/private'
-export const GET: RequestHandler = ({fetch}) => {
-	return new Response(String('Hello World!'))
+import {error} from '@sveltejs/kit'
+export const GET: RequestHandler = async ({fetch, url}) => {
+	const urlToHit = url.searchParams.get('url')
+
+	if (!urlToHit || !urlToHit.startsWith('http')) {
+		throw error(400, 'Missing valid `url` to screenshot')
+	}
+
+	const response = await fetch(`${PUBLIC_API_BASE_URL}/?url=${urlToHit}`, {
+		headers: {
+			'X-API-Key': API_KEY,
+		},
+	})
+
+	if (!response.body || !response.ok) {
+		throw error(500)
+	}
+
+	return new Response(response.body, {
+		headers: new Headers({
+			'Content-Type': 'image/png',
+		}),
+	})
 }
